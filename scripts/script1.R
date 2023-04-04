5cb7ef674f10fbef3a335590ad0354e5ac77f8f7
Kacper <kacpergruca@gmail.com>
2023-04-04 23:47:10 +0200

    Add config file and code

--- scripts/script1.R
+++ scripts/script1.R
@@ -1,53 +1,48 @@
 # install.packages("googlesheets4")
-#install.packages("gargle")
-
-rm(list=ls())
+# install.packages("gargle")
+# install.packages("yaml")
 
 library(googlesheets4)
 library(dplyr)
-# library(gargle)
+library(yaml)
 
-# # Google Sheet authentication
-# ## after running that part of code a browser window will open, asking you to grant permission to the googlesheets4 app. 
-# options(gargle_oauth_cache = ".secrets")
-# options(gargle_oauth_email = "kt.gruca@student.uw.edu.pl")
-# googlesheets4::gs4_auth(path = ".secrets/token")
+rm(list=ls())
 
-sheet_url1 <- "https://docs.google.com/spreadsheets/d/1QE03KZDkuKQxXUlxtvCtA95i69RqIJkWl7myFfGF_gw/edit?usp=sharing"
-sheet_url2 <- "https://docs.google.com/spreadsheets/d/1pleNQoKcDmoXbsp8qr0DvqFsOrCLPlPMzlHkhn6AHF8/edit?usp=sharing"
+# Load values from config
+config <- yaml.load_file("config.yaml")
 
-data1 <- read_sheet(sheet_url1)
-data2 <- read_sheet(sheet_url2)
+data1 <- read_sheet(config$SheetsURLs$sheetURL1)
+data2 <- read_sheet(config$SheetsURLs$sheetURL2)
+ddlDate <- config$Settings$ddlDate
+maxHoursDelayed <- config$Settings$maxHoursDelayed
+
 
 # function to create final DDL
-## DDL_date <- final deadline date, in the format "2023-04-06"
+## ddlDate <- final deadline date, in the format "2023-04-06"
 ## maxHoursDelayed <- the numeric number of hours a student may be late
 
-createFinalDDL <- function(DDL_date, maxHoursDelayed){
+createFinalDDL <- function(ddlDate, maxHoursDelayed){
   
-  DDL_date <- as.POSIXct(DDL_date, tz = "UTC")
+  ddlDate <- as.POSIXct(ddlDate, tz = "UTC")
   
   if(maxHoursDelayed == 0){
-    DDL_date <- DDL_date + 24*60*60 - 1
+    ddlDate <- ddlDate + 24*60*60 - 1
   } else{
-    DDL_date <- DDL_date + (24 + maxHoursDelayed) * 60 * 60
+    ddlDate <- ddlDate + (24 + maxHoursDelayed) * 60 * 60
   }
-  return(DDL_date)
+  return(ddlDate)
 }
-
-ddl_date <- createFinalDDL("2023-04-06", 0.5)
-ddl_date
 
 # function to filter answers
 ## only the first answer can be checkd
-## answer must be sent before the final DDL - ddl_date
+## answer must be sent before the final DDL - ddlDate
 
 ## df <- df to be filtered
-## ddl_date <- final ddl_date
+## ddlDate <- final ddlDate
 
-filterAnswers <- function(df, ddl_date){
+filterAnswers <- function(df, ddlDate){
   df <- df %>%
-    filter(df[[1]] <= ddl_date)
+    filter(df[[1]] <= ddlDate)
   
   df <- df %>%
     group_by(df[[5]]) %>%
@@ -57,6 +52,6 @@ filterAnswers <- function(df, ddl_date){
   return(df)
 }
 
-df_test <- filterAnswers(data1, ddl_date)
-df_test
+ddlDate <- createFinalDDL(ddlDate, maxHoursDelayed)
+data1 <- filterAnswers(data1, ddlDate)
 
