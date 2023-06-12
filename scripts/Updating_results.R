421439e245bfe7670aebbce35befaeea3d7f4829
Kacper <kacpergruca@gmail.com>
2023-06-12 10:33:36 +0200

    Add add_sum function

--- scripts/Updating_results.R
+++ scripts/Updating_results.R
@@ -1,11 +1,9 @@
-
 # take_max_points function
 # input:
 ## df
 # output:
 ## max_points to achieve in one test
-# assumption:
-## 1 task = 1 point
+# assumption - 1 task = 1 point
 
 take_max_points <- function(df){
   df_cols <- colnames(df)
@@ -21,6 +19,24 @@ take_max_points <- function(df){
   }
   return(max_points)
 }
+
+# add_sum function
+# Input:
+## df
+## max_points <- taken from previous function
+# Output:
+## df with column with calculated percentage points per student
+
+add_sum <- function(df, max_points){
+  last_col <- 2 + max_points
+  df$sum <- rowSums(Table3[, c(3:last_col)])
+  df$percentage <- round((df$sum / max_points) * 100, 0)
+  df <- subset(df, select = -c(sum))
+  return(df)
+}
+
+
+
 
 
 
